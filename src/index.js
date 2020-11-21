4f79c5f39063f3f7ebd155aaf99c908e84ceb082
Harish S.G <harishsg99@gmail.com>
2020-11-21 19:29:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+const https = require('https')
 
+const data = JSON.stringify({
+  payload: text
+})
+
+const options = {
+  hostname: x, 
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
